56ed9a01352a3fa681c74b8c501c744b90110676
MaksimGorkii <33204237+MaksimGorkii@users.noreply.github.com>
2020-01-14 14:39:55 -0500

    Add files via upload

---
+++ login app 13/config.js
@@ -0,0 +1,11 @@
+window.config = {
+   "model_": "AppConfig",
+   "id": 1,
+   "appName": "GOCA Student Sign Out ",
+   "version": "1.1",
+   "homepage": "https://docs.google.com/forms/d/e/1FAIpQLSeSdyEGwOTC6WhhBU9vZKsk-SVfXs293_rs9t08CdeFrVt_PQ/viewform?usp=sf_link",
+   "enableNavBttns": false,
+   "enableHomeBttn": false,
+   "enableLogoutBttn": false,
+   "kioskEnabled": true
+};
